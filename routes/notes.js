f51ca1d340685fb4dde2d4fb6059e204184f889f
Louis Antolin <ragnaman.exe@gmail.com>
2023-07-24 16:31:06 -0700

    typo

--- routes/notes.js
+++ routes/notes.js
@@ -20,7 +20,7 @@ notes.post('/', (req, res) => {
         id: uuidv4(),
     };
 
-    readAndAppend(newNote, './db/db.json');
+    readAndAppend(newNote, '../db/db.json');
     res.json(`Note added successfully 🚀`);
   } else {
     res.error('Error in adding note');
